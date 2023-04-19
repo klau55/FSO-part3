ce9aef9ea9f329a1af6489d3558c167f34e8185f
Klauß <kipatok@bk.ru>
2023-04-19 17:00:52 +0300

    3.5 done (if i post as JSON data, but it wasn't specified to be text as a must)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -63,4 +65,31 @@ app.delete('/api/persons/:id', (request, response) => {
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+})
+
+const getRandomId = (min, max) => {
+  min = Math.ceil(1);
+  max = Math.floor(1000);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if (!body) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: getRandomId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
